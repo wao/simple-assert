fb4a0219957197c5ad73753511ff5198f4e39e82
Yang Chen <mike.c.yang@gmail.com>
2015-05-04 10:43:25 +0800

    Add assertTrue and assertEquals

---
+++ src/main/java/info/thinkmore/Assert.java
@@ -0,0 +1,39 @@
+package info.thinkmore;
+
+import java.io.PrintStream;
+
+public class Assert {
+    static boolean enableAssert = true;
+    public static void enableAssert(){
+        enableAssert = true;
+    }
+
+    static public boolean isEnableAssert(){
+        return enableAssert;
+    }
+
+    static private void raiseException( String msg ){
+        AssertError ae = new AssertError( msg );
+        ae.printStackTrace( new PrintStream(System.err) );
+        throw ae;
+    }
+
+    public static class AssertError extends RuntimeException{
+        static final long serialVersionUID = 1000; 
+        public AssertError(String message){
+            super(message);
+        }
+    }
+    
+    static public void assertTrue( boolean exp ){
+        if( !exp ){
+          raiseException( "Assert failed" );
+        }
+    }
+
+    static public <T> void assertEquals( T left, T right ){
+        if( !left.equals(right) ){
+            raiseException( String.format( "Expect %s, but got %s", left.toString(), right.toString() ) );
+        }
+    }
+}
---
+++ src/test/java/info/thinkmore/AssertTest.java
@@ -0,0 +1,28 @@
+package info.thinkmore;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AssertTest {
+	@Test(expected= info.thinkmore.Assert.AssertError.class)
+	public void testAssertTrueFail() {
+		info.thinkmore.Assert.assertTrue(false);
+	}
+
+    @Test
+	public void testAssertTrue() {
+		info.thinkmore.Assert.assertTrue(true);
+    }
+
+    @Test
+	public void testAssertEquals() {
+		info.thinkmore.Assert.assertEquals(2, 2);
+    }
+
+	@Test(expected= info.thinkmore.Assert.AssertError.class)
+	public void testAssertEqualsFail() {
+		info.thinkmore.Assert.assertEquals(2, 3);
+    }
+
+}
